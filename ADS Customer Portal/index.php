2970fbff81caa2b9821ae6dd56d1b12fae1f2497
mingxunc <mingxun.chong@gmail.com>
2015-12-02 15:38:46 +0800

    Mingxun - First Commit

--- ADS Customer Portal/index.php
+++ ADS Customer Portal/index.php
@@ -1,9 +1,5 @@
 <?php
-//include('login.php'); // Includes Login Script
-//
-//if(!isset($_SESSION['loginID'])){
-//    header("location: index.php");
-//}
+
 ?>
 
 <!DOCTYPE html>
@@ -26,6 +22,7 @@
 
     <!-- Custom styles for this template -->
     <link href="theme.css" rel="stylesheet">
+    <link href="ADSConsumer.css" rel="stylesheet">
 
     <!-- jQuery and Bootstrap Javascript -->
     <script type="text/javascript" src="datatables/media/js/jquery.js"></script>
@@ -33,27 +30,24 @@
 </head>
 
 <body role="document">
+    <script type="text/javascript">
+        $(function(){
+            $("#header").load("header.php");
+        });
+    </script>
 
-<script type="text/javascript">
-    $(function(){
-        $("#header").load("header.php");
-        $("#footer").load("footer.php");
-    });
-</script>
+    <div id="header">
+    </div>
 
-<div id="header"></div>
+    <!--- Background Image Area ---->
+    <div class="jumbotron" id="background-area">
+        <div class="container theme-showcase" id="background-area-subcontainer" role="main">
+            <h4>Test</h4>
 
-<!-- Main jumbotron for a primary marketing message or call to action -->
-<div class="jumbotron">
-    <div class="container theme-showcase" role="main">
-        <h1>Theme example</h1>
-        <p>This is a template showcasing the optional theme stylesheet included in Bootstrap. Use it as a starting point to create something more unique by building on or modifying it.</p>
-        <div class="searchBox">
+            <!---- Search Area --->
             <form class="form-horizontal" role="form" action="searchResults.php" method="post">
-                <h4 style="padding-left: 20px;padding-top: 20px"> Search: </h4>
-                <div class="form-group" style="padding-left: 20px">
-<!--                    <label class="control-label col-sm-3 col-md-2" for="tenName1">Dormitories Search: </label>-->
-                    <div class="col-sm-3 col-md-2">
+                <div class="form-group" id="search-area">
+                    <div class="col-sm-3 col-md-4">
                         <input type="text" class="form-control" name="searchField" id="searchField" placeholder="Search...">
                     </div>
 
@@ -75,87 +69,14 @@
                             <option> Central</option>
                         </select>
                     </div>
-
                     <div class="col-sm-3 col-md-2">
-                        <button type="submit" class="btn btn-success" name="submit">Search</button>
+                        <button type="submit" class="btn btn-success" id="search-button" name="submit">Search</button>
                     </div>
                 </div>
             </form>
+            <!---- End Search Area --->
         </div>
     </div>
-</div>
-
-<!-- Marketing messaging and featurettes
-    ================================================== -->
-<!-- Wrap the rest of the page in another container to center all the content. -->
-
-<div class="container marketing">
-    <!-- Three columns of text below the carousel -->
-    <div class="row">
-        <div class="col-lg-4">
-            <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-            <h2>Heading</h2>
-            <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
-            <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-        </div><!-- /.col-lg-4 -->
-        <div class="col-lg-4">
-            <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-            <h2>Heading</h2>
-            <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
-            <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-        </div><!-- /.col-lg-4 -->
-        <div class="col-lg-4">
-            <img class="img-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-            <h2>Heading</h2>
-            <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-            <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-        </div><!-- /.col-lg-4 -->
-    </div><!-- /.row -->
-
-
-    <!-- START THE FEATURETTES -->
-
-    <hr class="featurette-divider">
-
-    <div class="row featurette">
-        <div class="col-md-7">
-            <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It'll blow your mind.</span></h2>
-            <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-        </div>
-        <div class="col-md-5">
-            <img class="featurette-image img-responsive center-block" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
-        </div>
-    </div>
-
-    <hr class="featurette-divider">
-
-    <div class="row featurette">
-        <div class="col-md-7 col-md-push-5">
-            <h2 class="featurette-heading">Oh yeah, it's that good. <span class="text-muted">See for yourself.</span></h2>
-            <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-        </div>
-        <div class="col-md-5 col-md-pull-7">
-            <img class="featurette-image img-responsive center-block" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
-        </div>
-    </div>
-
-    <hr class="featurette-divider">
-
-    <div class="row featurette">
-        <div class="col-md-7">
-            <h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
-            <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-        </div>
-        <div class="col-md-5">
-            <img class="featurette-image img-responsive center-block" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
-        </div>
-    </div>
-
-    <hr class="featurette-divider">
-
-    <!-- /END THE FEATURETTES -->
-
-    <div id="footer"></div>
-</div><!-- /.container -->
+    <!--- End Background Image Area ---->
 </body>
 </html>
